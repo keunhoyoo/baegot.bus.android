6ec34ee8e5610359789c7e8b817b62767bed80fa
keunho.yoo <keunho.yoo@gmail.com>
2017-06-16 17:40:17 +0900

    dd

--- app/src/main/java/bus/baegot/ykh/com/baegotbus/MainActivity.java
+++ app/src/main/java/bus/baegot/ykh/com/baegotbus/MainActivity.java
@@ -2,14 +2,42 @@ package bus.baegot.ykh.com.baegotbus;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.LinearLayout;
 
-public class MainActivity extends AppCompatActivity {
+import com.nhn.android.maps.NMapActivity;
+import com.nhn.android.maps.NMapController;
+import com.nhn.android.maps.NMapView;
+import com.nhn.android.maps.maplib.NGeoPoint;
 
+public class MainActivity extends NMapActivity {
 
-    sdsdfds
+    // API-KEY
+    private static final String API_KEY = "5NLJ2m79PG264wfrsGSi";
+
+    NMapView mapView = null;
+    NMapController mapController = null;
+    LinearLayout mapContainer = null;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //setContentView(R.layout.activity_main);
+
+        mapView = new NMapView(this);
+        mapController = mapView.getMapController();
+        mapView.setApiKey(API_KEY);
+
+        //((LinearLayout)findViewById(R.id.mapLayout)).addView(mapView);
+        setContentView(mapView);
+
+        mapView.setClickable(true);
+        mapView.setEnabled(true);
+        mapView.setFocusable(true);
+        mapView.setFocusableInTouchMode(true);
+        mapView.requestFocus();
+        mapController.setMapCenter(new NGeoPoint(126.978371, 37.566691), 11);
+
+
+
     }
 }
